f12a67b3cfc567db06e4fe9170b0445304bbcebb
GaganUnvi <38974523+GaganUnvi@users.noreply.github.com>
2020-07-07 17:17:17 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,28 +2,53 @@ import React, { useState } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [items, setItems] = useState('');
+
+  function addItem(e) {
+    e.preventDefault();
+    items !== ''
+      ? setTodos([{ name: items, completed: false }, ...todos])
+      : alert('Please enter an item');
+
+    console.log(todos);
+  }
+  function toggleClass(e, x) {
+    e.target.classList.toggle('complete');
+    const usr = [x];
+  }
 
   return (
     <div className="wrapper">
-      <button
+      {/* <button
         onClick={() =>
           setTodos([{ name: 'Buy apples', completed: false }, ...todos])
         }
       >
         Add todo
-      </button>
+      </button> */}
 
       <form>
-        <input type="text" className="todo-input" placeholder="Add item..." />
-        <button className="todo-button" type="submit">
+        <input
+          type="text"
+          onChange={(event) => setItems(event.target.value)}
+          className="todo-input"
+          placeholder="Add item..."
+        />
+        <button
+          onClick={(event) => addItem(event)}
+          className="todo-button"
+          type="submit"
+        >
           +
         </button>
       </form>
 
       <ul className="todo-list">
         {todos.map((todo) => (
-          <li className="complete">
-            <button className="done">&#10003;</button>
+          <li className={todo.completed ? 'complete' : 'uncompleted'}>
+            <button onClick={() => toggleClass(EventTarget)} className="done">
+              &#10003;
+            </button>
             <span>{todo.name}</span>
             <button className="delete">&#128465;</button>
           </li>
